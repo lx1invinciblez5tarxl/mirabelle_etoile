aeda1d929e27470b8ce082b974751252bc9e1406
lx1invinciblez5tarxl <48861488+lx1invinciblez5tarxl@users.noreply.github.com>
2021-01-29 10:40:28 -0500

    Delete custom.fancybox.js

--- js/custom.fancybox.js
+++
@@ -1,8 +0,0 @@
-$(".fancybox").fancybox({
-  helpers : { 
-   overlay: {
-    opacity: 0.8, // or the opacity you want 
-    css: {'background-color': '#ff0000'} // or your preferred hex color value
-   } // overlay 
-  } // helpers
-}); // fancybox
