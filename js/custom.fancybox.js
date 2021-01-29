e47b53f41132a614e31eccdc2e5e7af1a1375f89
lx1invinciblez5tarxl <48861488+lx1invinciblez5tarxl@users.noreply.github.com>
2021-01-29 10:39:57 -0500

    Add files via upload

---
+++ js/custom.fancybox.js
@@ -0,0 +1,8 @@
+$(".fancybox").fancybox({
+  helpers : { 
+   overlay: {
+    opacity: 0.8, // or the opacity you want 
+    css: {'background-color': '#ff0000'} // or your preferred hex color value
+   } // overlay 
+  } // helpers
+}); // fancybox
